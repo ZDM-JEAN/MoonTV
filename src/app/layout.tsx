c9c8516e584b5a0fd47c5e090cef01333bf0d75c
ZDM-JEAN <jean19830527@gmail.com>
2026-01-25 21:48:50 +0800

    Remove referrer property from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export async function generateMetadata(): Promise<Metadata> {
     title: siteName,
     description: '影视聚合',
     manifest: '/manifest.json',
-    referrer: 'no-referrer',
   };
 }
 
